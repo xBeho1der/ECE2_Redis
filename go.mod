2ab29e7c5eb99e28c19d1875cb4188dc69d9fa33
Jianteng Chen <2256687748@qq.com>
2023-08-26 15:47:30 +0800

    runable

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module redisdb 
 
 go 1.19
+require (
+	github.com/hdt3213/rdb v1.0.10
+)
--- main.go
+++ main.go
@@ -1,13 +1,65 @@
-// main.go
-
 package main
 
 import (
 	"fmt"
-	"redisdb/cmd"
+	"os"
+
+	"redisdb/config"
+	"redisdb/lib/logger"
+	"redisdb/lib/utils"
+	RedisServer "redisdb/redis/server"
+	"redisdb/tcp"
+	// "redisdb/config"
+	// "redisdb/lib/logger"
+	// "redisdb/lib/utils"
+	// RedisServer "redisdb/redis/server"
+	// "redisdb/tcp"
 )
 
+var banner = `
+ _____    _    ______   __  ____  _____ ____ ___ ____  
+| ____|  / \  / ___\ \ / / |  _ \| ____|  _ \_ _/ ___| 
+|  _|   / _ \ \___ \\ V /  | |_) |  _| | | | | |\___ \ 
+| |___ / ___ \ ___) || |   |  _ <| |___| |_| | | ___) |
+|_____/_/   \_\____/ |_|   |_| \_\_____|____/___|____/ 
+`
+
+var defaultProperties = &config.ServerProperties{
+	Bind:           "0.0.0.0",
+	Port:           6399,
+	AppendOnly:     false,
+	AppendFilename: "",
+	MaxClients:     1000,
+	RunID:          utils.RandString(40),
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
+
 func main() {
-	fmt.Println("Starting your project...")
-	cmd.Execute()
+	print(banner)
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "godis",
+		Ext:        "log",
+		TimeFormat: "2006-01-02",
+	})
+	configFilename := os.Getenv("CONFIG")
+	if configFilename == "" {
+		if fileExists("redis.conf") {
+			config.SetupConfig("redis.conf")
+		} else {
+			config.Properties = defaultProperties
+		}
+	} else {
+		config.SetupConfig(configFilename)
+	}
+	err := tcp.ListenAndServeWithSignal(&tcp.Config{
+		Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+	}, RedisServer.MakeHandler())
+	if err != nil {
+		logger.Error(err)
+	}
 }
